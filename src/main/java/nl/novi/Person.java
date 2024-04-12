89049f1d83ca1ad27fd02d8d197ab4afd1021206
BeheadedBadger <ZarradeRuijter@gmail.com>
2024-04-12 10:49:39 +0200

    Setup initial classes

---
+++ src/main/java/nl/novi/Person.java
@@ -0,0 +1,16 @@
+package nl.novi;
+
+import java.util.List;
+
+public class Person {
+    String name;
+    String middleName;
+    String lastName;
+    String sex;
+    int age;
+    Person mother;
+    Person father;
+    List<Person> siblings;
+    List<Person> children;
+    List<Pet> pets;
+}
---
+++ src/main/java/nl/novi/Pet.java
@@ -0,0 +1,8 @@
+package nl.novi;
+
+public class Pet {
+    String name;
+    int age;
+    String species;
+    Person owner;
+}
