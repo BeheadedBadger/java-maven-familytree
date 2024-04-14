2bd25731122b946b8acc6ef70f6083fa859b5097
BeheadedBadger <ZarradeRuijter@gmail.com>
2024-04-14 13:56:14 +0200

    Implementation of assignment and bonus

--- src/main/java/nl/novi/Pet.java
+++ src/main/java/nl/novi/Pet.java
@@ -5,4 +5,19 @@ public class Pet {
     int age;
     String species;
     Person owner;
+
+    public Pet(String name, int age, String species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+        owner.addPet(this);
+    }
+
+    @Override
+    public String toString() {
+        return owner + " has a " + species + " called " + name; }
 }
